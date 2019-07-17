d753264ee4402c414d44ee64646094bb5662ac57
michdol <michal.dolbniak@gmail.com>
2019-07-17 10:00:24 +0900

    Added drawing paddle

--- src/game.rs
+++ src/game.rs
@@ -4,22 +4,27 @@ use piston_window::types::Color;
 use crate::drawing::{draw_rectangle, draw_block};
 
 const BORDER_COLOR: Color = [0.741, 0.765, 0.78, 1.0];
+const PADDLE_COLOR: Color = [0.18, 0.80, 0.44, 1.0];
 
 
 pub struct Game {
     width: i32,
-    height: i32
+    height: i32,
+    paddle: Paddle
 }
 
 impl Game {
     pub fn new(width: i32, height: i32) -> Game {
         Game {
             width: width,
-            height: height
+            height: height,
+            paddle: Paddle::new((width / 2) - 1, height - 2)
         }
     }
 
     pub fn draw(&self, con: &Context, g: &mut G2d) {
+        self.paddle.draw(con, g);
+
         draw_rectangle(BORDER_COLOR, 0, 0, self.width, 1, con, g);
         draw_rectangle(BORDER_COLOR, 0, self.height - 1, self.width, 1, con, g);
         draw_rectangle(BORDER_COLOR, 0, 0, 1, self.height, con, g);
@@ -32,5 +37,26 @@ impl Game {
 
     pub fn key_pressed(&mut self, key: Key) {
         println!("Game::key_pressed");
+    }
+}
+
+pub struct Paddle {
+    x: i32,
+    y: i32,
+}
+
+impl Paddle {
+    pub fn new(x: i32, y: i32) -> Paddle {
+        Paddle {
+            x: x,
+            y: y,
+        }
+    }
+
+    pub fn draw(&self, con: &Context, g: &mut G2d) {
+        draw_block(PADDLE_COLOR, self.x, self.y, con, g);
+        draw_block(PADDLE_COLOR, self.x + 1, self.y, con, g);
+        draw_block(PADDLE_COLOR, self.x + 2, self.y, con, g);
+        draw_block(PADDLE_COLOR, self.x + 3, self.y, con, g);
     }
 }
